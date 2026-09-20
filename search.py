1d2e8af035065a2b6d9cdb3c9ceb32c7328c59fa
MrMatias305 <matiasmayor22xxii@gmail.com>
2026-09-20 08:51:20 +0530

    Modify search.py with sources structured

--- search.py
+++ search.py
@@ -19,13 +19,21 @@ def web_search(query, max_results=5):
     response = requests.post(url, json=payload)
     response.raise_for_status()
 
-    return response.json()["results"]
+    data = response.json()
+
+    results = []
+
+    for result in data["results"]:
+        results.append({
+            "title": result.get("title", ""),
+            "url": result.get("url", ""),
+            "content": result.get("content", "")
+        })
+
+    return results
 
 
 if __name__ == "__main__":
     results = web_search(query=input("Enter search query: "))
 
-    for index, result in enumerate(results, start=1):
-        print(f"\n[{index}] {result['title']}]")
-        print(result['url'])
-        print(result['content'][:500])
+    print(results)
